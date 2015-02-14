0c1ee47464c2958dae45e51048ed50184b601e21
BoxingP <boxingp@gmail.com>
2015-02-14 14:22:18 +0800

    C-16: update discount parsing to store same propert by calendar. Boxing

--- src/main/java/com/boxing/cart/unit/DiscountParsing.java
+++ src/main/java/com/boxing/cart/unit/DiscountParsing.java
@@ -1,42 +1,68 @@
 package com.boxing.cart.unit;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DiscountParsing {
-    private Map<ItemType, String[]> discountMap;
+    private Map<ItemType, Map<Calendar, Double>> discountMap;
+    private Map<Calendar, Double> electronicsDiscountMap;
+    private Map<Calendar, Double> foodDiscountMap;
+    private Map<Calendar, Double> dailyNecessitiesDiscountMap;
+    private Map<Calendar, Double> alcoholDiscountMap;
 
     public DiscountParsing() {
         this.discountMap = new HashMap<>();
+        this.electronicsDiscountMap=new HashMap<>();
+        this.foodDiscountMap=new HashMap<>();
+        this.dailyNecessitiesDiscountMap=new HashMap<>();
+        this.alcoholDiscountMap=new HashMap<>();
+
+        discountMap.put(ItemType.ELECTRONICS, electronicsDiscountMap);
+        discountMap.put(ItemType.FOOD, foodDiscountMap);
+        discountMap.put(ItemType.DAILY_NECESSITIES, dailyNecessitiesDiscountMap);
+        discountMap.put(ItemType.ALCOHOL, alcoholDiscountMap);
     }
 
-    public Map<ItemType, String[]> abstractDiscount(String discount) {
-        String[] dicountList = discount.split("\\n");
-        for (String discountElement :dicountList) {
+    public Map<ItemType, Map<Calendar, Double>> abstractDiscount(String discount) throws ParseException {
+        String[] discountList = discount.split("\\n");
+        for (String discountElement : discountList) {
             discountMap = parseDiscount(discountElement);
         }
         return discountMap;
     }
 
-    private Map<ItemType, String[]> parseDiscount(String discountElement) {
+    private Map<ItemType, Map<Calendar, Double>> parseDiscount(String discountElement) throws ParseException {
         String[] discountInformation = discountElement.split(" \\| ");
         switch (discountInformation[2]) {
             case "电子":
-                discountMap.put(ItemType.ELECTRONICS, new String[]{discountInformation[0], discountInformation[1]});
+                discountMap.put(ItemType.ELECTRONICS, storeCalendarDiscount(electronicsDiscountMap, discountInformation));
                 break;
             case "食品":
-                discountMap.put(ItemType.FOOD, new String[]{discountInformation[0], discountInformation[1]});
+                discountMap.put(ItemType.FOOD, storeCalendarDiscount(foodDiscountMap, discountInformation));
                 break;
             case "日用品":
-                discountMap.put(ItemType.DAILY_NECESSITIES, new String[]{discountInformation[0], discountInformation[1]});
+                discountMap.put(ItemType.DAILY_NECESSITIES, storeCalendarDiscount(dailyNecessitiesDiscountMap, discountInformation));
                 break;
             case "酒类":
-                discountMap.put(ItemType.ALCOHOL, new String[]{discountInformation[0], discountInformation[1]});
+                discountMap.put(ItemType.ALCOHOL, storeCalendarDiscount(alcoholDiscountMap, discountInformation));
                 break;
             default:
                 break;
         }
 
         return discountMap;
+    }
+
+    private Map<Calendar, Double> storeCalendarDiscount(Map<Calendar, Double> calendarDiscountMap, String[] discountInformation) throws ParseException {
+        Date date = new SimpleDateFormat("yyyy.MM.dd").parse(discountInformation[0]);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        calendarDiscountMap.put(calendar, Double.parseDouble(discountInformation[1]));
+        return calendarDiscountMap;
     }
 }
--- src/test/java/com/boxing/cart/unit/DiscountParsingTest.java
+++ src/test/java/com/boxing/cart/unit/DiscountParsingTest.java
@@ -1,11 +1,15 @@
 package com.boxing.cart.unit;
 
+import org.hamcrest.core.Is;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Map;
 
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class DiscountParsingTest {
@@ -17,19 +21,56 @@ public class DiscountParsingTest {
     }
 
     @Test
-    public void shouldDiscountString_return_discountMap() {
+    public void shouldDiscountString_return_discountMap() throws ParseException {
         String discount = "2013.11.11 | 0.7 | 电子";
-        Map<ItemType, String[]> discountMap = discountParsing.abstractDiscount(discount);
+        Map<ItemType, Map<Calendar, Double>> discountMap = discountParsing.abstractDiscount(discount);
 
-        assertThat(discountMap.get(ItemType.ELECTRONICS), is(new String[]{"2013.11.11", "0.7"}));
+        Date date13Nov11 = new SimpleDateFormat("yyyy.MM.dd").parse("2013.11.11");
+        Calendar calendar13Nov11 = Calendar.getInstance();
+        calendar13Nov11.setTime(date13Nov11);
+
+        assertThat(discountMap.get(ItemType.ELECTRONICS).get(calendar13Nov11), Is.<Object>is(0.7));
     }
 
     @Test
-    public void shouldDiscountStringContainsSeveral_return_discountMap() {
+    public void shouldDiscountStringContainsSeveral_return_discountMap() throws ParseException {
         String discount = "2013.11.11 | 0.7 | 电子\n2013.11.11 | 0.5 | 食品";
-        Map<ItemType, String[]> discountMap = discountParsing.abstractDiscount(discount);
+        Map<ItemType, Map<Calendar, Double>> discountMap = discountParsing.abstractDiscount(discount);
 
-        assertThat(discountMap.get(ItemType.ELECTRONICS), is(new String[]{"2013.11.11", "0.7"}));
-        assertThat(discountMap.get(ItemType.FOOD), is(new String[]{"2013.11.11", "0.5"}));
+        Date date13Nov11 = new SimpleDateFormat("yyyy.MM.dd").parse("2013.11.11");
+        Calendar calendar13Nov11 = Calendar.getInstance();
+        calendar13Nov11.setTime(date13Nov11);
+        
+        assertThat(discountMap.get(ItemType.ELECTRONICS).get(calendar13Nov11), Is.<Object>is(0.7));
+        assertThat(discountMap.get(ItemType.FOOD).get(calendar13Nov11), Is.<Object>is(0.5));
+    }
+
+    @Test
+    public void shouldDiscountStringSameProperty_return_discountMap() throws ParseException {
+        String discount = "2013.11.11 | 0.7 | 电子\n2013.11.12 | 0.7 | 电子";
+        Map<ItemType, Map<Calendar, Double>> discountMap = discountParsing.abstractDiscount(discount);
+
+        Date date13Nov11 = new SimpleDateFormat("yyyy.MM.dd").parse("2013.11.11");
+        Calendar calendar13Nov11 = Calendar.getInstance();
+        calendar13Nov11.setTime(date13Nov11);
+        
+        Date date13Nov12 = new SimpleDateFormat("yyyy.MM.dd").parse("2013.11.12");
+        Calendar calendar13Nov12 = Calendar.getInstance();
+        calendar13Nov12.setTime(date13Nov12);
+
+        assertThat(discountMap.get(ItemType.ELECTRONICS).get(calendar13Nov11), Is.<Object>is(0.7));
+        assertThat(discountMap.get(ItemType.ELECTRONICS).get(calendar13Nov12), Is.<Object>is(0.7));
+    }
+
+    @Test
+    public void shouldDiscountStringSamePropertySameCalendar_return_discountMap() throws ParseException {
+        String discount = "2013.11.11 | 0.7 | 电子\n2013.11.11 | 0.5 | 电子";
+        Map<ItemType, Map<Calendar, Double>> discountMap = discountParsing.abstractDiscount(discount);
+
+        Date date13Nov11 = new SimpleDateFormat("yyyy.MM.dd").parse("2013.11.11");
+        Calendar calendar13Nov11 = Calendar.getInstance();
+        calendar13Nov11.setTime(date13Nov11);
+
+        assertThat(discountMap.get(ItemType.ELECTRONICS).get(calendar13Nov11), Is.<Object>is(0.5));
     }
 }
