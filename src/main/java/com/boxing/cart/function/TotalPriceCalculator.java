38bd9d01d409253541ad58d4a7c88447dc7257f3
BoxingP <boxingp@gmail.com>
2015-02-25 15:45:46 +0800

    C-48: delete duplicate codes in TotalPriceCalculator. Boxing

--- src/main/java/com/boxing/cart/function/TotalPriceCalculator.java
+++ src/main/java/com/boxing/cart/function/TotalPriceCalculator.java
@@ -5,17 +5,21 @@ import com.boxing.cart.unit.calculator.CouponCalculator;
 import com.boxing.cart.unit.calculator.DiscountCalculator;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TotalPriceCalculator {
     public BigDecimal calculateTotalPrice(InputInformation inputInformation) {
         BigDecimal totalPrice = new BigDecimal("0");
 
-        Calculator discountCalculator = new DiscountCalculator();
-        totalPrice = discountCalculator.calculate(totalPrice, inputInformation);
-
-        Calculator couponCalculator = new CouponCalculator();
-        totalPrice = couponCalculator.calculate(totalPrice, inputInformation);
-
+        List<Calculator> calculators = new ArrayList<Calculator>();
+        calculators.add(new DiscountCalculator());
+        calculators.add(new CouponCalculator());
+        
+        for (Calculator calculator: calculators) {
+            totalPrice = calculator.calculate(totalPrice, inputInformation);
+        }
+        
         return totalPrice;
     }
 }
